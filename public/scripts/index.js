8d35ca867d8f86c7ce59ba04ae18ebf299a51901
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-10-09 11:08:29 -0300

    creating calendar

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,12 +1,34 @@
+function createList(list, item) {
+  const li = document.createElement('li');
+  li.innerHTML = `
+  <p>
+    <img src="${item.home_flag}" alt="${item.home_team_en} flag"></img>
+    <span>${item.home_team_en}</span>
+  </p>
+  <p>
+    <img src="${item.away_flag}" alt="${item.away_team_en} flag"></img>
+    <span>${item.away_team_en}</span>
+  </p>
+  <span>${new Date(item.local_date).toString()}</span>
+  `;
+  list.append(li);
+}
+
 window.onload = async () => {
-  const response = await fetch('https://interesting-sphenoid-chrysanthemum.glitch.me', {
-    headers: {
-      'Access-Control-Allow-Origin': 'cross-site'
-    }
-  });
-  const data = await response.json();
+  const calendarSection = document.querySelector('#calendar-section');
+  try {
+    const response = await fetch('https://interesting-sphenoid-chrysanthemum.glitch.me', {
+      headers: {
+        'Access-Control-Allow-Origin': 'cross-site'
+      }
+    });
+    const data = await response.json();
+    data.data.forEach((val) => {
+      createList(calendarSection, val)
+    });
+  } catch (error) {
+    const p = document.createElement('p');
+    p.innerText = 'Desculpe, erro no servidor';
+    calendarSection.append(p);
+  }
 };
-
-// fetch('http://api.cup2022.ir/api/v1/match', {
-
-// });
